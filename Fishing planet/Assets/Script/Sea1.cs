eee282c258b276f90ca8520117e1783845dd2cce
student <student>
2026-05-01 10:56:49 +0900

    Listと重さ付き確率を付与

--- Fishing planet/Assets/Script/Sea1.cs
+++ Fishing planet/Assets/Script/Sea1.cs
@@ -1,18 +1,17 @@
-using UnityEngine;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Sea1 : MonoBehaviour
 {   int rnd;
     List<string> Sea1List = new List<string>();
-
+    [SerializeField] Transform Lure;
     void Start()
     {
-
-        Sea1List.Add("イワシ");
-        Sea1List.Add("サバ");
-        Sea1List.Add("アジ");
-
+            Sea1List.Add("イワシ");
+            Sea1List.Add("サバ");
+            Sea1List.Add("アジ");
 
     }
 
@@ -31,9 +30,31 @@ public class Sea1 : MonoBehaviour
     {
         if (other.CompareTag("Lure"))
         {
+            float x = Lure.position.x / 3;
+
+            List<float> weights = new List<float>
+            {
+                50,30,10
+            };
+            rnd = GetRandomIndex(weights);
             Debug.Log("Lureが海から出た");
-            rnd = Random.Range(0, Sea1List.Count);
             Debug.Log(Sea1List[rnd] + "が釣れた");
         }
     }
+    int GetRandomIndex(List<float> weights)
+    {
+        float total = 0f;
+        foreach (var w in weights)
+            total += w;
+        float r = Random.Range(0, total);
+        for (int i = 0; i < weights.Count; i++)
+        {
+            r -= weights[i];
+            if (r <= 0)
+                return i;
+        }
+        return weights.Count - 1;
+    }
+
 }
+
