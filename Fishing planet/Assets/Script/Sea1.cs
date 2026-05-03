c01a204444b4115f2aa71f6388fd6bb8676e65b7
adeninnin <ademan28@iCloud.com>
2026-05-04 01:58:41 +0900

    距離により釣れる魚を違くした

--- Fishing planet/Assets/Script/Sea1.cs
+++ Fishing planet/Assets/Script/Sea1.cs
@@ -30,13 +30,22 @@ public class Sea1 : MonoBehaviour
     {
         if (other.CompareTag("Lure"))
         {
-            float x = Lure.position.x / 3;
-
-            List<float> weights = new List<float>
+            List<float> weights = new List<float> { 50, 30, 20 };
+            int area= Mathf.FloorToInt(Lure.position.x / 10f);
+            if (area == 0)
             {
-                50,30,10
-            };
+                weights = new List<float> { 70, 20, 0 };
+            }
+            else if(area == 1)
+            {
+                weights = new List<float> { 10, 60, 20 };
+            }
+            else if (area == 2)
+            {
+                weights = new List<float> { 0, 20, 60 };
+            }
             rnd = GetRandomIndex(weights);
+            Debug.Log("area: " + area);
             Debug.Log("Lureが海から出た");
             Debug.Log(Sea1List[rnd] + "が釣れた");
         }
@@ -47,7 +56,7 @@ public class Sea1 : MonoBehaviour
         foreach (var w in weights)
             total += w;
         float r = Random.Range(0, total);
-        for (int i = 0; i < weights.Count; i++)
+        for (int i = 0; i < weights.Count; i++)// 重みの合計からランダムな値を引いていき、0以下になったときのインデックスを返す
         {
             r -= weights[i];
             if (r <= 0)
