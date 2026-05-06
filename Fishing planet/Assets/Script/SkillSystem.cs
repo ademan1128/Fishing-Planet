ee5339ca9d5461bfbde7ee8532bd8affe592d7a1
ChubachiKakeru <sdt2595gp0012@edu.sca.ac.jp>
2026-05-06 17:34:02 +0900

    お金の取得とスキルを覚えられるかチェックするプログラムの実装

--- Fishing planet/Assets/Script/SkillSystem.cs
+++ Fishing planet/Assets/Script/SkillSystem.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public enum SkilltypeEnum
 {
     Cooltime1,
-    Colltime2,
+    Cooltime2,
     Pier1,
     Pier2,
     Fishinghook1,
@@ -42,7 +42,13 @@ public class SkillSystem : MonoBehaviour
         skills[(int)type] = true;
         SetSkillMoney(money);
         SetText();
-        CheckOnOff();
+        //CheckOnOff();
+    }
+
+    //スキルを覚えているかチェック
+    public bool IsSkill(SkilltypeEnum type)
+    {
+        return skills[(int)type];
     }
 
     //金額を減らす
@@ -51,12 +57,59 @@ public class SkillSystem : MonoBehaviour
         skillMoney -= money;
     }
 
+    //お金を取得
+    public int GetSkillMoney()
+    {
+        return skillMoney;
+    }
+
+    //スキルを覚えられるかチェック
+    public bool CanLearnSkill(SkilltypeEnum type,int spendMoney = 0)
+    {
+        //持ってる金額が足りない
+        if (skillMoney < 0)
+        {
+            return false;
+        }
+
+        //キャスト間隔Down２はキャスト間隔Down１を覚えてないとだめ
+        if(type == SkilltypeEnum.Cooltime2)
+        {
+            return skills[(int)SkilltypeEnum.Cooltime1];
+        }
+        //桟橋増加２は桟橋増加１を覚えてないとダメ
+        else if (type == SkilltypeEnum.Pier2)
+        {
+            return skills[(int)SkilltypeEnum.Pier1];
+        }
+        //釣り針増加２は釣り針増加１を覚えてないとダメ
+        else if (type == SkilltypeEnum.Fishinghook2)
+        {
+            return skills[(int)SkilltypeEnum.Fishinghook1];
+        }
+        //店舗強化は全てのスキルを覚えていなければダメ
+        else if (type == SkilltypeEnum.StrengthenStore)
+        {
+            return skills[(int)SkilltypeEnum.Cooltime1]&& skills[(int)SkilltypeEnum.Cooltime2] 
+                && skills[(int)SkilltypeEnum.Pier1] && skills[(int)SkilltypeEnum.Pier2] && skills[(int)SkilltypeEnum.Fishinghook1]
+                && skills[(int)SkilltypeEnum.Fishinghook2];
+        }
+        return true;
+    }
+
     //スキル毎にボタンのオン・オフをする処理を実行させる
+#if (Skillparameterrelationship)
     void CheckOnOff()
     {
         foreach (var skillParam in skillParams)
         {
-            //SkillParam.CheckButtonOnOff();
+            SkillParam.CheckButtonOnOff();
         }
     }
+
+    void SetText()
+    {
+        MoneyText.text = "金額：" + skillMoney;
+    }
+#endif
 }
