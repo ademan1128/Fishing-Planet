e6fa99edaecf500b22c4c647b610831f335a5e9d
student <student>
2026-05-16 19:50:57 +0900

    Result表示

--- Fishing planet/Assets/Script/SUI/Script/result.cs
+++ Fishing planet/Assets/Script/SUI/Script/result.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using System.Collections.Generic;
+using NUnit.Framework;
+
 
 public class result : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    List<string> results = new List<string>();
+    private void Start()
     {
-        
-    }
+        results.Add("マグロ");
+        results.Add("サメ");
+        results.Add("ブリ");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // 1つずつ表示
+        foreach (string fish in results)
+        {
+            Debug.Log(fish);
+        }
     }
 }
