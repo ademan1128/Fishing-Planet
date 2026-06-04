c0eec186f90af326cf47e7274e4768e08df9d170
adeninnin <ademan28@iCloud.com>
2026-06-05 01:07:46 +0900

    お金を追加、魚の大きさをenumで判定

--- Fishing planet/Assets/Script/HARUAKI/ScriptableObject/FishData.cs
+++ Fishing planet/Assets/Script/HARUAKI/ScriptableObject/FishData.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
-
+public enum FishSize
+{
+    Small,
+    Medium,
+    Large
+}
 [CreateAssetMenu(menuName = "Fish/FishData")]//インスペクターの右クリックでつくれるように
 public class FishDataSO : ScriptableObject
 {
-
+    public FishSize fishSize;
     public string fishName;
     public Sprite fishSprite;
     public int fishPrice;
